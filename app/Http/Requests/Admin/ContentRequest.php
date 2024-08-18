ef19ed6c00f55fe26ca4cf1c16880c633365ba3c
Tarikulat <tarikulat124@gmail.com>
2024-08-18 13:04:47 +0600

    update resource done

--- app/Http/Requests/Admin/ContentRequest.php
+++ app/Http/Requests/Admin/ContentRequest.php
@@ -4,7 +4,7 @@ namespace App\Http\Requests\Admin;
 
 use Illuminate\Foundation\Http\FormRequest;
 
-class BlogPostRequest extends FormRequest
+class ContentRequest extends FormRequest
 {
     public function authorize(): bool
     {
---
+++ app/Http/Resources/Admin/ContentResource.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Resources\Admin;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class ContentResource extends JsonResource
+{
+    public function toArray(Request $request): array
+    {
+        return [
+            "id"               => $this->id,
+            "title"            => $this->title,
+            "type"             => $this->type,
+            "content"          => $this->content,
+            "status"           => $this->status,
+            "image"            => Helper::getFilePath($this->img_path),
+            "created_at"       => $this->created_at,
+            "created_by"       => $this->whenLoaded('createdBy')
+        ];
+    }
+}
