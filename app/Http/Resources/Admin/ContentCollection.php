6802bfa6cb6b62b18534eea45b23bfa0f212f30e
Tarikulat <tarikulat124@gmail.com>
2024-08-18 13:09:49 +0600

    database design done

--- app/Http/Resources/Admin/ContentCollection.php
+++ app/Http/Resources/Admin/ContentCollection.php
@@ -5,7 +5,7 @@ namespace App\Http\Resources\Admin;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\ResourceCollection;
 
-class BlogPostCollection extends BaseCollection
+class ContentCollection extends BaseCollection
 {
 
 }
