bbf7cd0c2f2a0e5596dc04627c3645960bb5c2cf
Tarikulat <tarikulat124@gmail.com>
2024-08-19 18:08:41 +0600

    front resource and collection create done

---
+++ app/Http/Resources/Front/ContentCollection.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Resources\Front;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\ResourceCollection;
+
+class ContentCollection extends BaseCollection
+{
+
+}
